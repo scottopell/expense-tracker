b71f4e6bd2ec86784d976474ce4047d9b37484a1
Scott Opell <me@scottopell.com>
2016-04-14 16:29:27 -0400

    adds names to devise allowed params

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   resources :expenses, only: [ :index, :new, :create ]
 
   get 'dashboard' => 'misc#dashboard'
